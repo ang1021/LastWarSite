58c85fda77436a69eaab3886e35be50f97fe3b46
ang1021 <angelbakas@gmail.com>
2026-07-14 12:17:46 +0300

    Change WEB_APP_URL to new Google Apps Script URL
    Updated WEB_APP_URL to point to the new Google Apps Script endpoint.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const WEB_APP_URL = "https://dbt-alliance-dbt1.vercel.app";
+// The URL points to the google apps script that updates the spreadsheet
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxH1S-HyGLSpSOYtxXBkaunpSjksykQtGWRpDxfOyWK5Y4rZiLDrbZv4Qdoh1L5R-jpEA/exec";
 
 document.getElementById("sendButton").addEventListener("click", async () => {
 
